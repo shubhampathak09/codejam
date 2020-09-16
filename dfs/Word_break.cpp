7c3e7eb0786167530796cf30c09f753ae9a088e4
shubham <shubham.pathak@rediffmail.com>
2020-09-16 17:54:32 +0530

    word break recursion

---
+++ dfs/Word_break.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool check(string s1,set<string> s)
+{
+	
+	if(s.find(s1)!=s.end())
+	{
+		return true;
+	}
+	return false;
+}
+
+
+bool checkwordbreak(string s1,set<string>s)
+{
+	
+	if(s1=="")
+	return true;
+	
+	for(int i=1;i<=s1.length();i++)
+	{
+		//cout<<"here";
+	//	cout<<s1.substr(0,i)<<endl;
+		if(check(s1.substr(0,i),s)&&checkwordbreak(s1.substr(i,s1.length()),s))
+		{
+		//	cout<<s1.substr(0,i)<<"\n";
+			return true;
+		}
+		
+	}
+	
+	return false;
+}
+
+int main()
+{
+	
+	string s[]={"geek","geeks","gogo"};
+	
+	set<string>se;
+	
+	int si=sizeof(s)/sizeof(s[0]);
+//	cout<<si;
+	
+	
+	for(int i=0;i<si;i++)
+	se.insert(s[i]);
+	
+//	cout<<check("frog",se);
+//	cout<<check("geeks",se);
+//cout<<check("geek",se);
+//cout<<check("geeks",se);
+cout<<checkwordbreak("geekgeeks",se);
+
+}
