fa8d64c50d89fee0b5ab8bf41770bc70d790ff6e
shubham <shubham.pathak@rediffmail.com>
2020-10-23 10:26:48 +0530

    flywarsh

--- floyd_warshall.cpp
+++ floyd_warshall.cpp
@@ -6,6 +6,8 @@ using namespace std;
 #define V 4
 #define INF 100000007
 
+// floyd_warshall standarad impl,to find shortest path from every node to every other node multisource shprtest paths
+
 void floyd_warshall(int graph[V][V])
 {
 	
@@ -22,11 +24,12 @@ for(int k=0;k<V;k++)
 			}
 		}
 	}
-	}	
+	}
+	//o(n3)  see lectuers to understand more about the algo	
 	for(int i=0;i<V;i++)
 	{
 		for(int j=0;j<V;j++)
-		{
+		{ 
 			cout<<"shortest path from vertex "<<i<<" to "<<j<<"\t"<<graph[i][j]<<"\n";
 		}
 		cout<<"\n";
@@ -41,7 +44,8 @@ int main()
 //cout<<func(5);
 
 int graph[V][V]={{0,5,INF,10},{INF,0,3,INF},{INF,INF,0,1},{INF,INF,INF,0}};
-
+// adjacency matrix based impl can be extended to adjacency lists as well
+// check for interconversion b/w adjacency list and adjacnecy matrix:p
 floyd_warshall(graph);
 
  } 
