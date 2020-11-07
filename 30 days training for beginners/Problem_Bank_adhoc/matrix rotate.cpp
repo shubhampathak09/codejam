bb4344cd2f7ed9b63deccf374fc4eb770cc3a9f6
shubham <shubham.pathak@rediffmail.com>
2020-11-07 11:47:27 +0530

    corrected matrix rotaation for three scenarios 90 180 and 270 degree rotations

--- 30 days training for beginners/Problem_Bank_adhoc/matrix rotate.cpp
+++ 30 days training for beginners/Problem_Bank_adhoc/matrix rotate.cpp
@@ -1,21 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
+// fuck this shit
 
 
+// agorithhm
+/*
+for(int i=0;i<n;i++)
+{
+	for(int j-0;j<n;j++)
+	{
+		mat[i][j]=a[n-1-j][i];
+	}
+}
+*/
 int main()
 {
+/*	
+	7  8  1
+	3  4  5
+	1  2  3
+*/	
+/*	  1  3  7
+	  2  4  8
+	  3  5  1
+*/	
+/*	    3  2  1        for(int i;i<n;i++)
+	                   {
+	                   	for(int j=0;j<n;j++)
+	                   	{
+	                   		mat[i][j]=a[n-1-i][n-1-j]
+						   }
+					   }
+	    5  4  3 
+	    
+*/	    
+//	    1  8  7
+	        
+//	        7
+	// 180 mirror//
 	
 	
 	int a[3][3]={{7,8,1},{3,4,5},{1,2,3}};
 	int mat[3][3];
-	
-	for(int i=2;i>=0;i--)   // col
-	{
-		for(int j=0;j<=2;j++) // row
-		{
-			mat[i][j]=a[2-i][j];  //mat[2][0]=a[0][0]  mat[2][1]=a[0][1]  mat[2][2]=a[2][2];
-		}                          //mat[1][0]=a[1][0]
-	}
+//	
+//	for(int i=2;i>=0;i--)   // col
+//	{
+//		for(int j=0;j<=2;j++) // row
+//		{
+//			mat[i][j]=a[2-i][j];  //mat[2][0]=a[0][0]  mat[2][1]=a[0][1]  mat[2][2]=a[2][2];
+//		}                          //mat[1][0]=a[1][0]
+//	}
 	
 	for(int i=0;i<=2;i++)
 	{
@@ -27,7 +61,20 @@ int main()
 	}
 
 cout<<endl;
-	
+ 
+ int n=3;
+ 
+ for(int i=0;i<n;i++) // considering roation by 90
+
+{
+	for(int j=0;j<n;j++)
+	{
+	//	mat[i][j]=a[n-1-j][i];   //90 deg
+	//	mat[i][j]=a[n-1-i][n-1-j]; //180 degre
+		mat[i][j]=a[j][n-1-i];     //270 degree
+	}
+ } 
+ 	
    	
 	for(int i=0;i<=2;i++)   //first row->last col
 	{
