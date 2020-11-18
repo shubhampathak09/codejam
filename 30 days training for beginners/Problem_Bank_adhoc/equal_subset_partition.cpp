452e9d6fd57c8e209d5a4dafec8846d8dbc8add6
shubham <shubham.pathak@rediffmail.com>
2020-11-18 11:32:33 +0530

    equal subset partition

---
+++ 30 days training for beginners/Problem_Bank_adhoc/equal_subset_partition.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool equalsubset_partition(int ele[],int n,int target)
+{
+	
+	
+	
+}
+
+int main()
+{
+	int ele[]={1,5,11,5};
+	
+	int sum=0;
+	int n=sizeof(ele)/sizeof(ele[0]);
+	
+	for(int i=0;i<n;i++)
+	{
+		sum+=ele[i];
+	}
+	
+	if(sum&1)
+	{
+		cout<<"equal partiton not possible"
+	}
+	
+	else
+	{
+	
+	int target=sum/2;
+	equalsubset_partition()
+}
+}
