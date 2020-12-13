d8aad1282e2b8ad54e07803d1dcb3b52324ef1b2
shubham <shubham.pathak@rediffmail.com>
2020-12-13 11:47:42 +0530

    modified

--- 30 days training for beginners/Problem_Bank_adhoc/range_sum.cpp
+++ 30 days training for beginners/Problem_Bank_adhoc/range_sum.cpp
@@ -13,21 +13,21 @@ void build(int index,int low,int high)
 	
 	if(low==high)
 	{
-		seg[index]=a[low];
+		seg[index]=a[low];  //base condition of recursion will have a single element and make seg[index]->a[low]
 		return ;
 	}
 	
-	int mid=(low+high)/2;
-	build(2*index+1,low,mid);
+	int mid=(low+high)/2;   // mid low +(high-low)/2 to avoid overlflow in case
+	build(2*index+1,low,mid);   // left and right child
 	build(2*index+2,mid+1,high);
-	seg[index]=seg[2*index+1]+seg[2*index+2];
+	seg[index]=seg[2*index+1]+seg[2*index+2];  //storing the sum at root 
 	
 }
 
 int sum(int index,int low,int high,int l,int h)
 {
 	//completly inside                      l  low  high  h
-	if(low>=l&&high<=h)
+	if(low>=l&&high<=h)     // tree completly inside the range
 	return seg[index];
 	
 	if(low>h||high<l)  //    l  h  low  high     low high  l u
